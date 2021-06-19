928346a288f2c3cf41a6e74a31e0401eaed8e2c7
skarwaaaa <skarwaaaa@gmail.com>
2021-06-19 14:10:25 +0900

    admin設定

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  list_display = ('id', 'title', 'create_at')
+  list_display_links = ('title',)
+  ordering = ('-create_at',)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+  title = models.CharField('タイトル', max_length=50)
+  content = models.TextField('内容', max_length=1000)
+  create_at = models.DateTimeField(auto_now_add=True)
+  update_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.title
+  
