c4799ba479176f3489a5a0d786de3179718fd6e7
skarwaaaa <skarwaaaa@gmail.com>
2021-06-25 00:33:29 +0900

    投稿一覧ページ作成

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('post_create', views.PostCreate.as_view(), name='post_create'),
     path('post_detail/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
     path('post_update/<int:pk>', views.PostUpdate.as_view(), name='post_update'),
+    path('post_delete/<int:pk>', views.PostDelete.as_view(), name='post_delete'),
+    path('post_list', views.PostList.as_view(), name='post_list'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, resolve_url
 from django.http import HttpResponse
-from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, DeleteView, ListView
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm
@@ -32,3 +32,15 @@ class PostUpdate(UpdateView):
       def get_success_url(self):
             messages.info(self.request, '投稿内容を更新しました。')
             return resolve_url('myapp:post_detail', pk=self.kwargs['pk'])
+
+class PostDelete(DeleteView):
+      model = Post
+      def get_success_url(self):
+          messages.info(self.request, '投稿内容を削除しました')
+          return resolve_url('myapp:index')
+
+class PostList(ListView):
+  model = Post
+
+  def get_queryset(self):
+    return Post.objects.all().order_by('-created_at')
