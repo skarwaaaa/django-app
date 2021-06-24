20c968f8184adcc5c3a01cbe2eb813a6d76b463f
skarwaaaa <skarwaaaa@gmail.com>
2021-06-24 23:31:57 +0900

    投稿の更新処理

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -7,4 +7,6 @@ app_name = 'myapp'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('post_create', views.PostCreate.as_view(), name='post_create'),
+    path('post_detail/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('post_update/<int:pk>', views.PostUpdate.as_view(), name='post_update'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
 from django.http import HttpResponse
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm
-
+from django.contrib import messages
 
 class Index(TemplateView):
   template_name = 'myapp/index.html'
@@ -21,3 +21,14 @@ class PostCreate(CreateView):
   model = Post
   form_class = PostForm
   success_url = reverse_lazy('myapp:index')
+
+class PostDetail(DetailView):
+      model = Post
+
+class PostUpdate(UpdateView):
+      model = Post
+      form_class = PostForm
+
+      def get_success_url(self):
+            messages.info(self.request, '投稿内容を更新しました。')
+            return resolve_url('myapp:post_detail', pk=self.kwargs['pk'])
