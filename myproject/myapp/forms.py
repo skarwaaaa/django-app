0ddb5ef10ebc03c62a8a102ec5dc29c52947527e
skarwaaaa <skarwaaaa@gmail.com>
2021-06-27 01:28:46 +0900

    login画面作成

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Post
-
+from django.contrib.auth.forms import AuthenticationForm
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -8,6 +8,12 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'content')
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args,**kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = 'myapp:login'
+LOGIN_REDIRECT_URL = 'myapp:index'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
