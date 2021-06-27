85e763e1305c378dce65bb6e97f68851903e4bc7
skarwaaaa <skarwaaaa@gmail.com>
2021-06-27 18:54:48 +0900

    signup機能追加

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -17,3 +18,8 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, resolve_url
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView, DeleteView, ListView
 from .models import Post
 from django.urls import reverse_lazy
-from .forms import PostForm, LoginForm
+from .forms import PostForm, LoginForm, SignUpForm
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 
@@ -53,3 +53,12 @@ class Login(LoginView):
 
 class Logout(LogoutView):
   template_name = 'myapp/logout.html'
+
+class SignUp(CreateView):
+      form_class = SignUpForm
+      template_name = 'myapp/signup.html'
+      success_url = reverse_lazy('myapp:index')
+
+      def form_valid(self, form):
+            message.info(self.request, 'ユーザー登録をしました。')
+            return HttpResponseRedirect(self.get_seccess_url())
