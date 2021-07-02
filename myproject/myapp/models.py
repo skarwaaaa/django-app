bda9d4ea172de3005daf57863d73cbd308df9f1d
skarwaaaa <skarwaaaa@gmail.com>
2021-07-03 00:27:45 +0900

    pillowと画像保存フォルダ作成

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
   title = models.CharField('タイトル', max_length=50)
   content = models.TextField('内容', max_length=1000)
   category = models.ForeignKey('Category', on_delete=models.PROTECT)
+  thumbnail = models.ImageField(upload_to='images/', blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'myapp:login'
 LOGIN_REDIRECT_URL = 'myapp:index'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL ='/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
