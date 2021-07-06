c9ed9e827ad79a335c6c9b205520ce8926b24d13
skarwaaaa <skarwaaaa@gmail.com>
2021-07-06 17:36:12 +0900

    ナビバーにカテゴリ一覧を追加

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'myapp.context_processors.all_category',
             ],
         },
     },
